11407b0bcc0235bd72c24f99ddee8f27b60fa9a0
cobelu <imamac96@gmail.com>
2020-11-30 09:41:31 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# Connor Luckett for Joe Nelson - 2020
+# This code was based on the following:
+# https://projects.raspberrypi.org/en/projects/gpio-music-box/4
+# Buttons should be wired to pins 4 (5V) and 5 (GND)
+
+import pygame
+from gpiozero import Button
+
+
+def main():
+    """
+    Plays the given sound on button press.
+
+    """
+    # Note the location of the sound
+    sound_file = ""
+
+    # Initialize pygame and load the sound
+    pygame.init()
+    sound = pygame.mixer.Sound(sound_file)
+
+    # Link the sound to the button
+    btn = Button(4)
+    btn.when_pressed = sound.play
+
+
+if __name__ == '__main__':
+    main()
